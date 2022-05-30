235f6e383b58038301f1e67086f4232567d59920
sunil9813 <sunilpokhrel797@gmail.com>
2022-05-30 20:36:58 +0545

    01-birthday-reminder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
+import List from "./components/List"
+import { data } from "./components/data"
 
 function App() {
+  const [people, setPeople] = useState(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        {/* kate ota list xa bhanara check garrako */}
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        {/* clear garaxa sabai list or data lai */}
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
